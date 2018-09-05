724bdc5f98123a06d64f9ec9f3d8fac4028b637d
annwon19 <42421466+annwon19@users.noreply.github.com>
2018-09-05 11:26:17 -1000

    Add files via upload

---
+++ objectliteral.js
@@ -0,0 +1,18 @@
+var spicyAhi={
+fistType:"Ahi",
+sauce:"Sriracha & Mayo",
+price: 100.00,
+rice:"white",
+ounces: 8
+};
+var x=spicyAhi.sauce;
+window.alert(x);
+
+//ask user for the amount of spicy ahi to buy.
+var amount=prompt("amount to buy");
+//display the total amount of spicy the user will buy.
+var total=amount * spicyAhi.ounces;
+window.alert("buying"+totalOunces);
+//then display the total dollar amount.
+var totalDollar= amount*SpicyAhi. price;
+window.alert("your total price:"+ totalDollar);
